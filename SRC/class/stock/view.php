ca222197c09ec49e84894faba64b457f862629ed
itoatsushi <202406ap01_ito@x-ide.sakura.ne.jp>
2024-08-06 13:34:33 +0900

    修正

--- SRC/class/stock/view.php
+++ SRC/class/stock/view.php
@@ -255,14 +255,20 @@ function subStockEditView($param)
 			<tr>
 				<th>ランク</th>
 				<td>
-					<?php
-					for ($i = 0; $i < 5; $i++) {
-					?>
-						<input type="radio" name="rank" value="<?php print $i + 1; ?>" <?php if ($param["rank"] == $i) print ' checked="checked"'; ?> /> <?php print fnRankName($i); ?>
-					<?php
-					}
-					?>
-				</td>
+if (!$param["stockNo"]) {
+    $param["rank"] = 1;
+}
+for ($i = 0; $i < 5; $i++) {
+    $check = '';
+    if (($param["rank"] - 1) == $i) {
+        $check = 'checked = "checked"';
+    }
+?>
+<input type="radio" name="rank" value="<?php print $i + 1; ?>" <?php print $check; ?> /> <?php print fnRankName($i); ?>
+<?php
+}
+?>
+</td>
 			</tr>
 			<tr>
 				<th>物件名<span class="red">（必須）</span></th>
