97c30a798c72f7858c471d1e6cb5d4652756421a
itoatsushi <202406ap01_ito@x-ide.sakura.ne.jp>
2024-08-06 14:23:42 +0900

    追加

--- SRC/class/stock/view.php
+++ SRC/class/stock/view.php
@@ -354,12 +354,12 @@ function subStockEditView($param)
 					for ($i = 0; $i < 6; $i++) {
 					?>
 						<br />
-<?php
+
 						if (!$param["stockNo"]) {
 						$param["how"] = 1;
 						}
-						for ($i = 0; $i < 6; $i++) { $check='' ; if (($param["how"] - 1)==$i) { $check='checked = "checked"' ; }
-							?>
+						for ($i = 0; $i < 6; $i++) { $check='' ; if (($param["how"] - 1)==$i) { $check='checked = "checked"' ; } ?>
+
 							<br />
 							<input type="radio" name="how" value="<?php print $i + 1; ?>" <?php print $check; ?> /> <?php print fnHowName($i); ?>
 						<?php
